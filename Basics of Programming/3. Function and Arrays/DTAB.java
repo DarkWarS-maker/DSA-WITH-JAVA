839c0959e6bd09645da02a6bcac17b2c1e68e08e
DarkWarS <ds1830054@gmail.com>
2021-07-10 22:01:37 +0530

    Easy

---
+++ Basics of Programming/3. Function and Arrays/DTAB.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class DTAB {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int decimalToAnyBase(int n, int b) {
+        int ans = 0;
+        int mult = 1;
+        while (n != 0) {
+            int rem = n % b;
+            n /= b;
+            ans += rem * mult;
+            mult *= 10;
+        }
+        return ans;
+    }
+
+    public static void main(String asrgs[]) {
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        int ans = decimalToAnyBase(n, b);
+        System.out.println(ans);
+
+    }
+
+}
