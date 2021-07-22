f5fd978629a405bcd5eafd2936b4b8250ed62319
DarkWarS <ds1830054@gmail.com>
2021-07-22 21:02:38 +0530

    Revised

--- 2. Recursion and Backtracking/5. Recursion-backtracking/TargetSumSubSet.java
+++ 2. Recursion and Backtracking/5. Recursion-backtracking/TargetSumSubSet.java
@@ -4,6 +4,25 @@ public class TargetSumSubSet {
 
     public static Scanner sc=new Scanner(System.in);
 
+    public static void printAns(int arr[],int idx,int n,String asf,int tar,int sum){
+
+        if(idx==n){ 
+            if(sum==tar){
+                System.out.print(asf);
+                System.out.println();
+            }
+            return;
+        }
+
+        if(sum>tar){ //nice
+            return;
+        }
+
+        printAns(arr,idx+1, n, asf+arr[idx]+" ", tar,sum+arr[idx]);
+        printAns(arr, idx+1, n, asf, tar,sum);
+
+    }
+
     public static void main(String[] args) {
         int n=sc.nextInt();
         int arr[]=new int[n];
@@ -11,7 +30,7 @@ public class TargetSumSubSet {
             arr[i]=sc.nextInt();
         }
         int tar=sc.nextInt();
-        printAns()
+        printAns(arr,0,n,"",tar,0);
         
     }
     
