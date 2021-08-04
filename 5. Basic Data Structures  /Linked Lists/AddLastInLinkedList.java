3986c46deb5ce3e2d2f7c587ae16fe1d2e2b68b2
DarkWarS <ds1830054@gmail.com>
2021-08-04 21:52:45 +0530

    Easy

---
+++ 5. Basic Data Structures  /Linked Lists/AddLastInLinkedList.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+public class AddLastInLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+      }
+    
+      public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+    
+        void addLast(int val) {
+          // Write your code here
+          if(size==0){
+              Node temp=new Node(val);
+              temp.next=null;
+              head=tail=temp;
+              size++;
+          }
+          else{
+              Node temp=new Node(val);
+              tail.next=temp;
+              tail=temp;
+              size++;
+          }
+          
+        }
+      }
+    
+      public static void testList(LinkedList list) {
+        for (Node temp = list.head; temp != null; temp = temp.next) {
+          System.out.println(temp.data);
+        }
+        System.out.println(list.size);
+    
+        if (list.size > 0) {
+          System.out.println(list.tail.data);
+        } 
+      }
+      public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+    
+        String str = br.readLine();
+        while(str.equals("quit") == false){
+          if(str.startsWith("addLast")){
+            int val = Integer.parseInt(str.split(" ")[1]);
+            list.addLast(val);
+          } 
+          str = br.readLine();
+        }
+    
+        testList(list);
+      }
+    
+}
