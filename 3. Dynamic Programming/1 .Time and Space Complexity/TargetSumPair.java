2ef763c336724f29342258dbc323b278938a19a4
DarkWarS <ds1830054@gmail.com>
2021-08-14 23:09:02 +0530

    Revision

---
+++ 3. Dynamic Programming/1 .Time and Space Complexity/TargetSumPair.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class TargetSumPair {
+    public static void targetSumPair(int[] arr, int target){
+        //write your code here
+        
+        Arrays.sort(arr);
+        int le=0;
+        int ri=arr.length-1;
+        
+        while(le<ri){
+            
+           
+            if(arr[le]+arr[ri]<target){
+                le++;
+            }
+            else  if(arr[le]+arr[ri]>target){
+                ri--;
+              
+            }
+            else{
+                  System.out.println(arr[le]+ ", "+arr[ri]);
+                le++;
+                ri--;
+               
+            }
+        }
+        
+        
+    
+      }
+      public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i < n; i++){
+          arr[i] = scn.nextInt();
+        }
+        int target = scn.nextInt();
+        targetSumPair(arr,target);
+      }
+    
+    
+}
