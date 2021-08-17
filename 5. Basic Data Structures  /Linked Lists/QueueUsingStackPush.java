a00bc4e64555469876b68a173c4049ae31a688fa
DarkWarS <ds1830054@gmail.com>
2021-08-17 21:48:28 +0530

    Revised

---
+++ 5. Basic Data Structures  /Linked Lists/QueueUsingStackPush.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class QueueUsingStackPush {
+    LinkedList<Integer> st = new LinkedList<>();
+    LinkedList<Integer> temp = new LinkedList<>();
+    
+    int peekEle = 0;
+    
+    public QueueUsingStackPush() {
+        
+    }
+    
+    //O(1)
+    public void push(int x) {
+        if(st.size() == 0) peekEle = x;
+        st.addFirst(x);    
+    }
+    
+    private void transfer(LinkedList<Integer> st1,LinkedList<Integer> st2){
+        while(st1.size() != 0){
+            st2.addFirst(st1.removeFirst());
+        }
+    }
+    
+    //O(n)
+    public int pop() {
+        transfer(st,temp);
+        int rEle = temp.removeFirst();
+        
+        while(temp.size() != 0) this.push(temp.removeFirst());
+        return rEle;
+        
+    }
+    
+    public int peek() {
+        return peekEle;
+    }
+    
+    public boolean empty() {
+        return st.size() == 0;    
+    }
+    
+}
