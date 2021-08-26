511aadf1d38667d7c98c2d269f9e389beddfc005
DarkWarS <ds1830054@gmail.com>
2021-08-26 23:41:58 +0530

    Revised

--- 5. Basic Data Structures  /Stacks and Queues/BalancedBracket.java
+++ 5. Basic Data Structures  /Stacks and Queues/BalancedBracket.java
@@ -6,7 +6,7 @@ public class BalancedBracket {
         String str=sc.nextLine();
         
         Stack<Character> st=new Stack<>();
-        
+
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
             if(ch=='(' || ch=='[' || ch=='{'){
